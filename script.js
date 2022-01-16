274e77525cbbfd0aaa15584cf1d922383fcff5a1
Vaugert <lindstromfabian@gmail.com>
2022-01-16 15:33:00 +0000

    add run game script

--- script.js
+++ script.js
@@ -16,7 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+ /**
+  * the main game "loop", called when the script is first loaded
+  * and after the users answer has beenn processed 
+  */
 function runGame() {
+    let num1 = Math.floor(Math.random() *25) + 1;
+    let num2 = Math.floor(Math.random() *25) + 1;
 
 }
 
