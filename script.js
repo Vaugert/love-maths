97c181cc03365c7c61f667d920112e9b63fe4e3b
Vaugert <lindstromfabian@gmail.com>
2022-01-17 19:20:31 +0000

    add structure to division script

--- script.js
+++ script.js
@@ -34,9 +34,19 @@ function runGame(gameType) {
     document.getElementById("answer-box").value="";
     document.getElementById("answer-box").focus();
 
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    let numDiv = Math.floor(Math.random() * 25) ;   
+    let numDiv2 = Math.floor(Math.random() * 25) ;
+    
+    
+        
+     
+    
+         
+    
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -44,6 +54,8 @@ function runGame(gameType) {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion (num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(numDiv, numDiv2);
     }
     
     else {
@@ -91,6 +103,8 @@ function calculateCorrectAnswer() {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     }
     
     else {
@@ -140,4 +154,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand2").textContent = operand2 ;
     document.getElementById('operator').textContent = "x";
     
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2; 
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/"
 }
